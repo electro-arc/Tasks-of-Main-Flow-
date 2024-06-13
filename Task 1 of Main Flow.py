6cb62a85ee3800c4e0e012de9a05cad0f442bfe9
electro-arc <bishnirai163@gmail.com>
2024-06-13 15:42:10 +0530

    Add files via upload

---
+++ Task 1 of Main Flow.py
@@ -0,0 +1,43 @@
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+print("List:", my_list)
+
+# Adding an element to the list
+my_list.append(6)
+print("List after adding an element:", my_list)
+
+# Removing an element from the list
+my_list.remove(3)
+print("List after removing an element:", my_list)
+
+# Modifying an element in the list
+my_list[2] = 10
+print("List after modifying an element:", my_list)
+
+# Creating a dictionary
+my_dict = {"apple": 1, "banana": 2, "cherry": 3}
+print("Dictionary:", my_dict)
+
+# Adding a key-value pair to the dictionary
+my_dict["date"] = 4
+print("Dictionary after adding a key-value pair:", my_dict)
+
+# Removing a key-value pair from the dictionary
+del my_dict["banana"]
+print("Dictionary after removing a key-value pair:", my_dict)
+
+# Modifying a value in the dictionary
+my_dict["apple"] = 5
+print("Dictionary after modifying a value:", my_dict)
+
+# Creating a set
+my_set = {1, 2, 3, 4, 5}
+print("Set:", my_set)
+
+# Adding an element to the set
+my_set.add(6)
+print("Set after adding an element:", my_set)
+
+# Removing an element from the set
+my_set.remove(3)
+print("Set after removing an element:", my_set)
